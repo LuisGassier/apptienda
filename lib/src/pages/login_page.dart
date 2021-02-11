0878fd290ddd800769367537e14a730b422855aa
LuisGassier <56460777+LuisGassier@users.noreply.github.com>
2021-02-11 14:58:47 -0600

    Color

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -148,13 +148,10 @@ class LoginPage extends StatelessWidget {
       height: size.height * 0.4,
       width: double.infinity,
       decoration: BoxDecoration(
-          // gradient: LinearGradient(
-          //   colors: <Color> [
-          //     Color.fromRGBO(63, 63, 156, 1.0),
-          //     Color.fromRGBO(90, 70, 178, 1.0)
-          //   ]
-          // )
-          ),
+          gradient: LinearGradient(colors: <Color>[
+        Color.fromRGBO(63, 63, 156, 1.0),
+        Color.fromRGBO(90, 70, 178, 1.0)
+      ])),
     );
 
     final circulo = Container(
@@ -179,7 +176,7 @@ class LoginPage extends StatelessWidget {
             children: <Widget>[
               Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
               SizedBox(height: 10.0, width: double.infinity),
-              Text('Tienda Herrera',
+              Text('Tienda',
                   style: TextStyle(color: Colors.white, fontSize: 25.0))
             ],
           ),
--- lib/src/pages/registro_page.dart
+++ lib/src/pages/registro_page.dart
@@ -180,7 +180,7 @@ class RegistroPage extends StatelessWidget {
             children: <Widget>[
               Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
               SizedBox(height: 10.0, width: double.infinity),
-              Text('Tienda Herrera',
+              Text('Tienda',
                   style: TextStyle(color: Colors.white, fontSize: 25.0))
             ],
           ),
